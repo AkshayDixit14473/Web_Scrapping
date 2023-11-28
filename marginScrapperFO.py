37ce998155fe35838084911cf2ed31ef9df5d506
Akshay <81112869+AkshayDixit14473@users.noreply.github.com>
2023-11-28 10:27:51 +0530

    Update marginScrapperFO.py

--- marginScrapperFO.py
+++ marginScrapperFO.py
@@ -44,11 +44,11 @@ headers = {
     'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
-    'ufp': 'e00b00b3615c917ee30e3184f206e55b',
+    'ufp': 'serial no',
 }
 
 response = requests.get(
-    'https://www.connect2nsccl.com/risk-fo/tm-margins-summery-level-I/JSON:JSON',
+    'url',
     cookies=cookies,
     headers=headers,
 )
