7e2a9ee827ec70d6e749d6ae41088b771d883a24
Akshay <81112869+AkshayDixit14473@users.noreply.github.com>
2023-11-28 10:18:29 +0530

    Update twillioAlerts.py

--- twillioAlerts.py
+++ twillioAlerts.py
@@ -6,10 +6,10 @@ from twilio.twiml.voice_response import Say
 
 class twilioAlerts:
   def __init__(self) -> None:
-    self.account_sid = "AC3f02358943d45ff1a128a1081b9bc168"
-    self.auth_token = "81fbc8daf9575683fc0783830a575f8d"
-    self.number = '+12565008146'
-    self.randKey = "$9avZYt!B!"
+    self.account_sid = "id"
+    self.auth_token = "token"
+    self.number = 'number'
+    self.randKey = "key"
     self.twilio_client = Client(self.account_sid, self.auth_token)
 
   def sendMsgAlert(self, recipient, msg = "This is a alert"):
@@ -28,5 +28,5 @@ class twilioAlerts:
 
 
 # myAlert = Alerts()
-# myAlert.sendCallAlert("+919057243109", "The margin is above 80%.")
-# myAlert.sendMsgAlert("+919057243109", "The margin is above 80%.")
+# myAlert.sendCallAlert("no1", "The margin is above 80%.")
+# myAlert.sendMsgAlert("no1", "The margin is above 80%.")
